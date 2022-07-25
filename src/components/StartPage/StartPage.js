87e2be754a79db0fd26fe9ea56c9751442097dea
JeanJesus <31626353+JeanJesusTI@users.noreply.github.com>
2022-07-25 18:02:19 -0300

    Add files via upload

--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -4,7 +4,7 @@ const StartPage = ({ startGame }) => {
   return (
     <div className="start">
         <h1>Guess The Word!</h1>
-        <p>Click in the button bellow to start the game</p>
+        <p>Click the button bellow to start the game</p>
         <button onClick={startGame}>Start Game</button>
     </div>
   )
